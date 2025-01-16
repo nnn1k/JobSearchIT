1aceb54ede1b47c7061b2ba3f8387f2bfe376393
Ванек <zhurama2016@gmail.com>
2025-01-16 18:06:15 +0300

    front for login

---
+++ frontend/templates/pages/auth/js_login.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $("#switchLoginForm_1").click(function() {
+        $("#container_worker").toggle();
+        $("#container_employer").toggle();
+    });
+    $("#switchLoginForm_2").click(function() {
+        $("#container_worker").toggle();
+        $("#container_employer").toggle();
+    });
+});
