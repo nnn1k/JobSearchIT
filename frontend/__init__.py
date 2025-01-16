0e26cff0119de1762ea232c92f83b68cf1b12e19
Ванек <zhurama2016@gmail.com>
2025-01-16 23:00:34 +0300

    hueta

--- frontend/__init__.py
+++ frontend/__init__.py
@@ -1,8 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
 
 templates = Jinja2Templates(directory="frontend")
 router = APIRouter(include_in_schema=False)
+
+
+@router.get("/")
+async def read_root(request: Request):
+    return templates.TemplateResponse("/templates/pages/auth/html_login.html", {"request": request})
 
 
 
