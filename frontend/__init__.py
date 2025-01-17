8ab4de925974b4abd4ad978298a1399a176c4461
Ванек <zhurama2016@gmail.com>
2025-01-17 23:30:12 +0300

    hueta

--- frontend/__init__.py
+++ frontend/__init__.py
@@ -7,8 +7,17 @@ templates = Jinja2Templates(directory='frontend/templates')
 router = APIRouter(include_in_schema=False)
 
 @router.get("/")
-def read_root(request: Request):
+def login_page(request: Request):
     return templates.TemplateResponse("/pages/auth/html_login.html", {"request": request})
+
+@router.get("/signup/worker")
+def reg_worker(request: Request):
+    return templates.TemplateResponse("/pages/auth/html_reg_worker.html", {"request": request})
+
+
+@router.get("/signup/employer")
+def reg_employer(request: Request):
+    return templates.TemplateResponse("/pages/auth/html_reg_employer.html", {"request": request})
 
 
 
