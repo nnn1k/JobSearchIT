36adb34750a8366d2d789aa1473004cee7fd7f1f
nnn1k <nikitos.elidu@gmail.com>
2025-01-18 18:58:11 +0300

    add redis + check code

--- backend/utils/redis_func.py
+++ backend/utils/redis_func.py
@@ -1,7 +1,7 @@
 import redis
 
 def create_redis_client():
-    connect_string = 'redis://default:VXBhCLDuIQmAbzuQdfNxMZhedkGQTPvw@monorail.proxy.rlwy.net:26453'
+    connect_string = 'redis://default:MXnVzZUbxetVwLWVzvWSTLMIacVdknim@monorail.proxy.rlwy.net:44060'
     client = redis.from_url(connect_string)
     return client
 
@@ -9,3 +9,4 @@ def get_code_from_redis(user_type, user_id):
     redis_client = create_redis_client()
     new_code = redis_client.hget(f'{user_type}:{user_id}', 'code').decode('utf-8')
     return new_code
+
