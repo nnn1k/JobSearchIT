35c8d7772c72628a73d42c9f4f1ac20ed15d2f55
Ванек <zhurama2016@gmail.com>
2025-01-18 21:12:54 +0300

    hueta

--- frontend/templates/pages/auth/js_login.js
+++ frontend/templates/pages/auth/js_login.js
@@ -1,3 +1,5 @@
+import {apiUrl, makeRequest} from '../../../js/utils.js';
+
 $(document).ready(function() {
     $("#switchLoginForm_1").click(function() {
         $("#container_worker").toggle();
@@ -8,3 +10,17 @@ $(document).ready(function() {
         $("#container_employer").toggle();
     });
 });
+
+async function login_worker() {
+    const email = document.getElementById("login").value
+    const password = document.getElementById("password").value
+    const postResponse = await makeRequest({
+        method: 'POST',
+        url: '/api/auth/workers/login',
+        data: {
+            email,
+            password
+        }
+    })
+}
+window.login_worker = login_worker;
