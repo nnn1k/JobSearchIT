e1bea79fd7e45976029ef40c895f2fc45d533ff8
nnn1k <nikitos.elidu@gmail.com>
2025-01-20 00:17:30 +0300

    ..

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -1,8 +1,6 @@
 from fastapi import APIRouter
-from .users import worker_router, auth_router, employer_router
+from .users import router as users_router
 
 router = APIRouter(prefix="/api")
 
-router.include_router(auth_router)
-router.include_router(worker_router)
-router.include_router(employer_router)
+router.include_router(users_router)
--- backend/api/users/__init__.py
+++ backend/api/users/__init__.py
@@ -1,3 +1,10 @@
+from fastapi import APIRouter
+
 from .auth import router as auth_router
 from .workers.views import router as worker_router
 from .employers.views import router as employer_router
+
+router = APIRouter()
+router.include_router(auth_router)
+router.include_router(worker_router)
+router.include_router(employer_router)
