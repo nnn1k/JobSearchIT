aa0e5756cc238083b9cfa175890f50f431daf74a
Ванек <zhurama2016@gmail.com>
2025-01-23 06:33:57 +0300

    hueta

--- frontend/js/functions_for_profile.js
+++ frontend/js/functions_for_profile.js
@@ -1,7 +1,7 @@
 export function hide_field(row, row_edit) {
     const field_array = [['row_by_name', 'row_by_edit_name'], ['row_by_surname', 'row_by_edit_surname'], ['row_by_patronymic', 'row_by_edit_patronymic'],
         ['row_by_phone', 'row_by_edit_phone'], ['row_by_сompany', 'row_by_edit_сompany'],
-        ['row_by_birthday', 'row_by_edit_birthday'],['row_by_gender', 'row_by_edit_gender'],['row_by_city', 'row_by_edit_city']]
+        ['row_by_birthday', 'row_by_edit_birthday'],['row_by_city', 'row_by_edit_city']]
     for (let i = 0; i < field_array.length; i++) {
         const index = field_array[i].indexOf(row);
         if (index !== -1) {
@@ -18,7 +18,6 @@ export function hide_field(row, row_edit) {
     for (let i = 0; i < new_field_array.length; i++) {
         if ($(`#${new_field_array[i][0]}`).is(':hidden')) {
             $(`#${new_field_array[i][0]}`).toggle(300);
-
             $(`#${new_field_array[i][1]}`).toggle(300);
 
         }
