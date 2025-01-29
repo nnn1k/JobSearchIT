c3b9e4c2baa5d6dbe638b20049529a1f9ad61bbc
nnn1k <nikitos.elidu@gmail.com>
2025-01-29 06:06:16 +0300

    refactoring

--- backend/utils/other/check_func.py
+++ backend/utils/other/check_func.py
@@ -1,11 +1,11 @@
 from backend.api.companies.schemas import CompanySchema
-from backend.api.users.employers.schemas import EmployerSchema
-from backend.api.users.workers.schemas import WorkerSchema
+from backend.api.users.employers.schemas import EmployerSchema, EmployerResponseSchema
+from backend.api.users.workers.schemas import WorkerSchema, WorkerResponseSchema
 from backend.schemas.global_schema import GlobalSchema
 
 
 def check_employer_can_update(user: WorkerSchema | EmployerSchema, obj: GlobalSchema) -> bool:
-    if not isinstance(user, EmployerSchema):
+    if not isinstance(user, EmployerResponseSchema):
         return False
     if isinstance(obj, CompanySchema):
         return user.company_id == obj.id
@@ -14,7 +14,7 @@ def check_employer_can_update(user: WorkerSchema | EmployerSchema, obj: GlobalSc
     return False
 
 def check_worker_can_update(user: WorkerSchema | EmployerSchema, obj: GlobalSchema) -> bool:
-    if not isinstance(user, WorkerSchema):
+    if not isinstance(user, WorkerResponseSchema):
         return False
     if hasattr(obj, 'worker_id'):
         return user.id == obj.worker_id
