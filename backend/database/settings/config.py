e719faac6533c3ce357438545389baca0c7c1c8a
nnn1k <nikitos.elidu@gmail.com>
2025-01-30 12:19:20 +0300

    add func get_user_type

--- backend/database/settings/config.py
+++ backend/database/settings/config.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
 
     @property
     def ASYNC_DB_URL(self):
-        return 'postgresql+asyncpg://mayvwrto:r2avBxAmtEhvx7-LLfTekKidIs0VTOl8@mouse.db.elephantsql.com/mayvwrto'
+        return 'postgresql+asyncpg://postgres:HXjGMYSHgliDTHdJyjPWuXYunubKtacO@monorail.proxy.rlwy.net:52369/railway'
 
 
 settings = Settings()
