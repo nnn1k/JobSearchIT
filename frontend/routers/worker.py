51ed5986226f766c2f91caae59719d3d41f0c0af
nnn1k <nikitos.elidu@gmail.com>
2025-02-08 15:51:52 +0300

    add vacancy skills (front and back)

--- frontend/routers/worker.py
+++ frontend/routers/worker.py
@@ -13,12 +13,14 @@ def my_account_worker(request: Request):
         return RedirectResponse(url="/login")
     return templates.TemplateResponse("/pages/worker/profile/profile.html", {"request": request})
 
+
 @router.get('/resumes')
 def resume_get(request: Request):
     user_type = request.cookies.get("user_type")
     if user_type != "worker":
         return RedirectResponse(url="/login")
     return templates.TemplateResponse("/pages/worker/resume/all/resume.html", {"request": request})
+
 
 @router.get('/resumes/{resume_id}/edit')
 def resume_edit(request: Request):
