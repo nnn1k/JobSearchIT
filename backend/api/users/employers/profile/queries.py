d956585a534c0bfaefb5c635cad52ee149c24c6a
nnn1k <nikitos.elidu@gmail.com>
2025-03-12 22:21:11 +0300

    add selectinload vacancies

--- backend/api/users/employers/profile/queries.py
+++ backend/api/users/employers/profile/queries.py
@@ -2,7 +2,7 @@ from sqlalchemy import select, update
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload, selectinload
 
-from backend.core.database.models.employer import EmployersOrm
+from backend.core.database.models.employer import CompaniesOrm, EmployersOrm
 
 from backend.core.schemas.models.employer.employer_schema import EmployerResponseSchema
 
@@ -10,12 +10,12 @@ from backend.core.utils.exc import employer_not_found_exc
 
 
 async def get_employer_by_id_queries(employer_id: int, session: AsyncSession) -> EmployerResponseSchema:
-    stmt = await session.execute(
+    result = await session.execute(
         select(EmployersOrm)
-        .options(joinedload(EmployersOrm.company))
+        .options(joinedload(EmployersOrm.company).selectinload(CompaniesOrm.vacancies))
         .filter_by(id=int(employer_id))
     )
-    employer = stmt.scalars().one_or_none()
+    employer = result.scalars().one_or_none()
     if not employer:
         raise employer_not_found_exc
 
@@ -24,14 +24,14 @@ async def get_employer_by_id_queries(employer_id: int, session: AsyncSession) ->
 
 
 async def update_employer_by_id_queries(employer_id: int, session: AsyncSession, **kwargs):
-    stmt = await session.execute(
+    result = await session.execute(
         update(EmployersOrm)
         .filter_by(id=int(employer_id))
         .values(**kwargs)
         .returning(EmployersOrm)
-        .options(selectinload(EmployersOrm.company))
+        .options(joinedload(EmployersOrm.company).selectinload(CompaniesOrm.vacancies))
     )
-    employer = stmt.scalars().one_or_none()
+    employer = result.scalars().one_or_none()
     if not employer:
         raise employer_not_found_exc
 
