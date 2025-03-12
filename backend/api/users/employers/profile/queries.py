f8704dfb8fd3ea6f4fc6954b0eeb25414f840bff
nnn1k <nikitos.elidu@gmail.com>
2025-03-12 22:39:41 +0300

    add selectinload vacancies

--- backend/api/users/employers/profile/queries.py
+++ backend/api/users/employers/profile/queries.py
@@ -2,7 +2,7 @@ from sqlalchemy import select, update
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload, selectinload
 
-from backend.core.database.models.employer import CompaniesOrm, EmployersOrm
+from backend.core.database.models.employer import CompaniesOrm, EmployersOrm, VacanciesOrm
 
 from backend.core.schemas.models.employer.employer_schema import EmployerResponseSchema
 
@@ -12,7 +12,7 @@ from backend.core.utils.exc import employer_not_found_exc
 async def get_employer_by_id_queries(employer_id: int, session: AsyncSession) -> EmployerResponseSchema:
     result = await session.execute(
         select(EmployersOrm)
-        .options(joinedload(EmployersOrm.company).selectinload(CompaniesOrm.vacancies))
+        .options(joinedload(EmployersOrm.company).selectinload(CompaniesOrm.vacancies).joinedload(VacanciesOrm.profession))
         .filter_by(id=int(employer_id))
     )
     employer = result.scalars().one_or_none()
