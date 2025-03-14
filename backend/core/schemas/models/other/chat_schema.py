1ddbb925531b97ae1ea06ef5c1b208664c41cb10
nnn1k <nikitos.elidu@gmail.com>
2025-03-14 03:45:06 +0300

    add last_message

--- backend/core/schemas/models/other/chat_schema.py
+++ backend/core/schemas/models/other/chat_schema.py
@@ -1,7 +1,8 @@
 import datetime
-from typing import Optional
+from typing import Any, Dict, List, Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator, model_validator
+from pydantic_core.core_schema import ValidationInfo
 
 from backend.core.schemas.global_schema import GlobalSchema
 
@@ -9,7 +10,22 @@ from backend.core.schemas.global_schema import GlobalSchema
 class ChatSchema(GlobalSchema):
     response_id: int
     messages: Optional[list['MessageSchema']]
+    last_message: Optional['MessageSchema'] = None
     response: 'ResponseSchema'
+
+    @field_validator('messages', mode='after')
+    def validate_messages(cls, messages: Optional[List['MessageSchema']]) -> Optional[List['MessageSchema']]:
+        # Просто возвращаем messages, чтобы валидатор не пытался вернуть словарь
+        return messages
+
+    @model_validator(mode='after')
+    def update_last_message(cls, values: 'ChatSchema') -> 'ChatSchema':
+        # Обновляем last_message на основе messages
+        if values.messages:
+            values.last_message = values.messages[-1]
+        else:
+            values.last_message = None
+        return values
 
 class MessageSchema(GlobalSchema):
     chat_id: int
